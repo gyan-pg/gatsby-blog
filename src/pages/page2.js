d1e361be033393d629249a13245a86124504f36a
takuya hatanaka <trhchop0723@gmail.com>
2022-08-17 11:01:27 +0900

    page2作成

---
+++ src/pages/page2.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Page2 = () => {
+  return (
+    <>
+      <div>Page2</div>
+      <Link to="/">ホームに戻る</Link>
+    </>
+  )
+}
+
+export default Page2
